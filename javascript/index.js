69fc533756751501832ced8f3f0801a16000e440
celine-kanagwa <celinekanagwa@gmail.com>
2021-07-05 16:05:51 +0200

    new message

--- javascript/index.js
+++ javascript/index.js
@@ -4,13 +4,15 @@ var mobilenav = document.getElementById("mobilenav");
 mobilenav.style.left = "-100%";
 
 btn.onclick = function(){
+
     if(mobilenav.style.left == "-100%"){
         mobilenav.style.left = "50%";
-        btn.src = "image/icon-close.svg";
+        btn.src = "images/icon-close.svg";
     }
 
     else{
         mobilenav.style.left = "-100%";
-        btn.src = "image/icon-hamburger.svg";
+        btn.src = "images/icon-hamburger.svg";
     }
 }
+
